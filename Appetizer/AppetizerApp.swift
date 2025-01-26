6983354c0060aa487d52e4c2d5117e880f899dda
ayemereodia2 <odiadavid2@gmail.com>
2025-01-26 16:48:47 -0400

    Initial Commit

---
+++ Appetizer/AppetizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizerApp.swift
+//  Appetizer
+//
+//  Created by ANDELA on 26/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppetizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
